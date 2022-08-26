0a15920a5ce26d220db88ae8fed832c9e7151d74
feifan <shaoyangfan@inspur.com>
2022-08-26 19:07:57 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <ff-button type="primary" @click="fu">按钮</ff-button>
+      <ff-button type="success">登录</ff-button>
+      <ff-button type="info">删除</ff-button>
+      <ff-button type="warning">删除</ff-button>
+      <ff-button type="danger">删除</ff-button>
+    </div>
+    <div class="row">
+      <ff-button plain type="primary">按钮</ff-button>
+      <ff-button plain type="success">登录</ff-button>
+      <ff-button plain type="info">删除</ff-button>
+      <ff-button plain type="warning">删除</ff-button>
+      <ff-button plain type="danger">删除</ff-button>
+    </div>
+    <div class="row">
+      <ff-button plain round type="primary">按钮</ff-button>
+      <ff-button plain round type="success">登录</ff-button>
+      <ff-button plain round type="info">删除</ff-button>
+      <ff-button plain round type="warning">删除</ff-button>
+      <ff-button plain round type="danger">删除</ff-button>
+    </div>
+
+    <div class="row">
+      <ff-button plain circle type="primary">按钮</ff-button>
+      <ff-button plain circle type="success">登录</ff-button>
+      <ff-button plain circle type="info">删除</ff-button>
+      <ff-button plain circle type="warning">删除</ff-button>
+      <ff-button plain circle type="danger">删除</ff-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    fu () {
+      console.log(123)
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.row {
+  margin-bottom: 20px;
+
+  .ff-button {
+    margin-right: 20px;
+  }
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import FfButton from './components/button.vue'
 Vue.config.productionTip = false
+
+Vue.component(FfButton.name,FfButton)
 
 new Vue({
   render: h => h(App),
